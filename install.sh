e83a28ff376b34409d4db91df9a35787a4117533
Walter de Araujo <walteraxalta@gmail.com>
2024-04-27 14:12:45 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -93,7 +93,7 @@ const config = {
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
-    './shadcn-001/app/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
 	],
   prefix: \"\",
